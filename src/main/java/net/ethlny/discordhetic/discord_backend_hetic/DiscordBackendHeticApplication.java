4ad97faa268a9a74b93bee5622e68756a3410651
Setsudashi <cryptiareta@gmail.com>
2025-03-31 11:33:05 +0200

    first commit

---
+++ src/main/java/net/ethlny/discordhetic/discord_backend_hetic/DiscordBackendHeticApplication.java
@@ -0,0 +1,13 @@
+package net.ethlny.discordhetic.discord_backend_hetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordBackendHeticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordBackendHeticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ethlny/discordhetic/discord_backend_hetic/DiscordBackendHeticApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ethlny.discordhetic.discord_backend_hetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordBackendHeticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
